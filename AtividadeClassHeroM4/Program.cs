059fff9eb5b0aafde0b1cfec0e144998e977b6e5
Manu <emmanuellymadeira@gmailcom>
2025-08-27 22:16:51 -0300

    Desafio missao 4
    Criei o projeto e fiz o script da missao 4

---
+++ AtividadeClassHeroM4/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] num = new int[10];
+        int maior = int.MinValue;
+        for (int i = 0; i < num.Length; i++)
+        {
+            Console.Write($"Digite um numero {i + 1}: ");
+            num[i] = int.Parse(Console.ReadLine());
+
+            if (num[i] > maior)
+                maior = num[i];
+        }
+        Console.WriteLine($"Maior Numero digitado é {maior}" );
+    }
+}
